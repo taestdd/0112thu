cc6221026cca5f184cbd5f88d7d7b02361abafe7
hong <rlaxoxo5252@gmail.com>
2023-01-12 21:47:13 +0900

    입력값 띄우기 성공!

--- app/src/main/java/com/opensurvey/a0112thu/MainActivity.kt
+++ app/src/main/java/com/opensurvey/a0112thu/MainActivity.kt
@@ -2,14 +2,23 @@ package com.opensurvey.a0112thu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//   확인버튼이 눌리면? 이벤트 달아주기
+
+        enterBtn.setOnClickListener {
+            val inputContent = contentEdt.text.toString()
+
+            resultTxt.text = inputContent
 
 
+
+        }
 
     }
 }
